439f680798881c96dabec313e6ef56a8699b7add
Ethan D'Almeida <ethanalmeida184@gmail.com>
2025-06-28 10:06:34 +0300

    fix: correct app title and format code in main.dart

--- trip_expense_tracker/lib/main.dart
+++ trip_expense_tracker/lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
-
-void main(){
+void main() {
   runApp(const TripExpenseTrackerApp());
 }
 
 class TripExpenseTrackerApp extends StatelessWidget {
   const TripExpenseTrackerApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tally App',
+      title: 'Trip Expense Tracker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        ),
-        home: const HomeScreen(),
-        debugShowCheckedModeBanner: false,
-        
-      );
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
