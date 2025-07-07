24929cc2ca4d17e2990cd2cd42d63918c82b8222
Ethan D'Almeida <ethanalmeida184@gmail.com>
2025-07-07 18:01:29 +0300

    feat: enhance Create Purchase screen to support editing existing purchases

--- lib/screens/create_purchase_screen.dart
+++ lib/screens/create_purchase_screen.dart
@@ -5,7 +5,8 @@ import 'package:flutter/services.dart';
 
 class CreatePurchaseScreen extends StatefulWidget {
   final Group group;
-  const CreatePurchaseScreen({super.key, required this.group});
+  final Map<String, dynamic>? purchase;
+  const CreatePurchaseScreen({super.key, required this.group, this.purchase});
 
   @override
   State<CreatePurchaseScreen> createState() => _CreatePurchaseScreenState();
@@ -17,6 +18,22 @@ class _CreatePurchaseScreenState extends State<CreatePurchaseScreen> {
   List<String> selectedPayees = [];
   String splitMethod = 'equal';
   Map<String, double> amounts = {};
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.purchase != null) {
+      purchaseName = widget.purchase!['name'] ?? '';
+      selectedPayees = List<String>.from(widget.purchase!['payees'] ?? []);
+      splitMethod = widget.purchase!['splitMethod'] ?? 'equal';
+
+      if (widget.purchase!['amounts'] != null) {
+        final rawAmounts = widget.purchase!['amounts'] as Map<String, dynamic>;
+        amounts = rawAmounts.map((key, value) => 
+          MapEntry(key, (value is num) ? value.toDouble() : 0.0));
+      }
+    }
+  }
 
   Future<Map<String, String>> _getUserNames(List<String> userIds) async {
     final usersCollection = FirebaseService().firestore.collection('users');
@@ -31,7 +48,7 @@ class _CreatePurchaseScreenState extends State<CreatePurchaseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Create Purchase')),
+      appBar: AppBar(title: Text(widget.purchase != null ? 'Edit Purchase' : 'Create Purchase')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -40,6 +57,7 @@ class _CreatePurchaseScreenState extends State<CreatePurchaseScreen> {
             children: [
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Purchase Name'),
+                initialValue: purchaseName,
                 onChanged: (val) => setState(() => purchaseName = val),
                 validator: (val) => val == null || val.isEmpty ? 'Enter a name' : null,
               ),
@@ -84,10 +102,13 @@ class _CreatePurchaseScreenState extends State<CreatePurchaseScreen> {
               if (splitMethod == 'equal')
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Total Amount'),
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
                   inputFormatters: [
                     FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d{0,2}')),
                   ],
+                  initialValue: selectedPayees.isNotEmpty && amounts.isNotEmpty 
+                      ? (amounts.values.first * selectedPayees.length).toStringAsFixed(2)
+                      : '',
                   onChanged: (val) {
                     final amount = double.tryParse(val) ?? 0.0;
                     setState(() {
@@ -127,17 +148,28 @@ class _CreatePurchaseScreenState extends State<CreatePurchaseScreen> {
               ElevatedButton(
                 onPressed: () async {
                   if (_formKey.currentState!.validate()) {
-                    await FirebaseService().addPurchaseToGroup(
-                      groupId: widget.group.id,
-                      name: purchaseName,
-                      payees: selectedPayees,
-                      amounts: amounts,
-                      splitMethod: splitMethod,
-                    );
+                    if (widget.purchase != null) {
+                      await FirebaseService().updatePurchase(
+                        groupId: widget.group.id,
+                        purchaseId: widget.purchase!['id'],
+                        name: purchaseName,
+                        payees: selectedPayees,
+                        amounts: amounts,
+                        splitMethod: splitMethod,
+                      );
+                    } else {
+                      await FirebaseService().addPurchaseToGroup(
+                        groupId: widget.group.id,
+                        name: purchaseName,
+                        payees: selectedPayees,
+                        amounts: amounts,
+                        splitMethod: splitMethod,
+                      );
+                    }
                     Navigator.pop(context);
                   }
                 },
-                child: const Text('Save Purchase'),
+                child: Text(widget.purchase != null ? 'Update Purchase' : 'Save Purchase'),
               ),
             ],
           ),
